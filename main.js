7fd730878f918d9cf92dcf2c5362f6e00cf5397f
Gilmar David <gdavid.dubon@gmail.com>
2021-02-10 13:31:44 -0600

    comienzo

--- main.js
+++ main.js
@@ -1,23 +1,23 @@
-var miDBAlumnos= openDatabase('dbAlumnos','1.0','Aplicacion de Alumnos',5*1024*1024);
+var miDBAlumnos = openDatabase('dbAlumnos','1.0','Aplicacion de Alumnos',5*1024*1024);
 window.id = 0;
 if(!miDBAlumnos){
     alert("Elnavegador no soporta Web SQL");
 }
-
 var appVue = new Vue({
-
-    el: '#appAlumnos',
-    data: {
+    el:'#appAlumnos',
+    data:{
         alumno:{
-            idAlumno  : 0,
+            idAlumno    : 0,
             codigo      : '',
-            nombre       :'',
-            direccion         : '', 
-            municipio         : '', 
-            departamento      : '',    
-            telefono          : '',
-            fechanacimiento   : '',      
-            sexo              : '' 
+            nombre      : '',
+            direccion   : '',
+            municipio   : '',
+            departamento: '',
+            telefono    : '',
+            f_nacimiento: '',
+            sexo        : '',
+            img         : '/images/No-image-available.png',
+            img2        : '/images/No-image-available.png',
         },
         alumnos:[]
     },
@@ -27,15 +27,22 @@ var appVue = new Vue({
              * BD Web SQL
              */
             miDBAlumnos.transaction(tran=>{
-                tran.executeSql('INSERT INTO alumnos(idAlumno,codigo,nombre,direccion,municipio,departamento,telefono,fechanacimiento,sexo) VALUES(?,?,?,?,?,?,?,?,?) ',
-                    [++id,this.alumno.codigo,this.alumno.nombre,this.alumno.direccion, this.alumno.municipio,this.alumno.departamento,this.alumno.telefono, this.alumno.fechanacimiento,this.alumno.sexo]);
+                tran.executeSql('INSERT INTO alumnos(idAlumno,codigo,nombre,direccion,municipio,departamento,telefono,f_nacimiento,sexo,img) VALUES(?,?,?,?,?,?,?,?,?,?) ',
+                    [++id,this.alumno.codigo,this.alumno.nombre,this.alumno.direccion,this.alumno.municipio,this.alumno.departamento,this.alumno.telefono,this.alumno.f_nacimiento,this.alumno.sexo,this.alumno.img]);
                 this.obtenerAlumnos();
                 this.limpiar();
             }, err=>{
                 console.log( err );
             });
         },
-
+        obtenerImg(e){
+            //IMG 1
+            let rutaTemp = URL.createObjectURL(e.target.files[0]);
+            this.alumno.img = rutaTemp;
+            //IMG2
+            /*rutaTemp = URL.createObjectURL(e.target.files[1]);
+            this.producto.img2 = rutaTemp;*/
+        },
         obtenerAlumnos(){
             miDBAlumnos.transaction(tran=>{
                 tran.executeSql('SELECT * FROM alumnos',[],(index,data)=>{
@@ -46,26 +53,24 @@ var appVue = new Vue({
                 console.log( err );
             });
         },
-        
-        mostrarAlumno(aln){
-            this.alumno = aln;
+        mostrarAlumno(alu){
+            this.alumno = alu;
         },
-
         limpiar(){
             this.alumno.codigo='';
-            this.alumno.nombre='';
+            this.alumno.nombre='';  
             this.alumno.direccion='';
             this.alumno.municipio='';
             this.alumno.departamento='';
             this.alumno.telefono='';
-            this.alumno.fechanacimiento='';
+            this.alumno.f_nacimiento='';
             this.alumno.sexo='';
-
+            this.alumno.img='';
         }
     },
     created(){
         miDBAlumnos.transaction(tran=>{
-            tran.executeSql('CREATE TABLE IF NOT EXISTS alumnos(idAlumno int PRIMARY KEY NOT NULL, codigo varchar(10), nombre varchar(60),direccion varchar(60),municipio varchar(50),departamento varchar(50),telefono varchar(10),fechanacimiento date(), sexo varchar(20))');
+            tran.executeSql('CREATE TABLE IF NOT EXISTS alumnos(idAlumno int PRIMARY KEY NOT NULL, codigo varchar(10), nombre varchar(65), direccion varchar(60), municipio varchar(65), departamento varchar(65),telefono varchar(8),f_nacimiento varchar(65),sexo varchar(1), img varchar(100))');
         }, err=>{
             console.log( err );
         });
