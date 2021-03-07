ebda3085699b8408c925a8639f62a87382de2a04
Elba Cristina Rodriguez Perdomo <elbacristinarodriguezperdomo@gmail.com>
2021-03-07 01:06:38 -0600

    desplegable de lecturas

--- lecturas.js
+++ lecturas.js
@@ -7,14 +7,17 @@ Vue.component('component-lecturas',{
         status : false,
         error  : false,
         buscar : "",
-        lectura:{
+        lecturas:{
             idLectura      : 0,
-            clientes : {
+            cliente : {
                 id : 0,
                 label : ''
             },
-            Lecturas_contador    : '',
-            Pago                 : '',
+            codigo       : '',
+            fecha        : '',
+            lanterior    : '',
+            lactual      : '',
+            pago         : ''
  
         },
         lecturas:[],
@@ -22,16 +25,16 @@ Vue.component('component-lecturas',{
     }
 },
 methods:{
-    buscandoCliente(){
+    buscandoLectura(){
         this.lecturas = this.lecturas.filter((element,index,clientes) => element.nombre.toUpperCase().indexOf(this.buscar.toUpperCase())>=0 || element.codigo.toUpperCase().indexOf(this.buscar.toUpperCase())>=0 );
         if( this.buscar.length<=0){
             this.obtenerDatos();
         }
     },
-    buscandoCodigoCliente(store){
+    buscandoCodigoLectura(store){
         let buscarCodigo = new Promise( (resolver,rechazar)=>{
             let index = store.index("codigo"),
-                data = index.get(this.lectura.codigo);
+                data = index.get(this.lecturas.codigo);
             data.onsuccess=evt=>{
                 resolver(data);
             };
@@ -45,13 +48,13 @@ methods:{
         let store = this.abrirStore("tblLecturas",'readwrite'),
             duplicado = false;
         if( this.accion=='nuevo' ){
-            this.lectura.idLectura = generarIdUnicoDesdeFecha();
+            this.lecturas.idLectura = generarIdUnicoDesdeFecha();
             
-            let data = await this.buscandoCodigoCliente(store);
+            let data = await this.buscandoCodigoLectura(store);
             duplicado = data.result!=undefined;
         }
         if( duplicado==false){
-            let query = store.put(this.lectura);
+            let query = store.put(this.lecturas);
             query.onsuccess=event=>{
                 this.obtenerDatos();
                 this.limpiar();
@@ -83,7 +86,7 @@ methods:{
         let store = this.abrirStore('tblLecturas','readonly'),
         data = store.getAll();
     data.onsuccess=resp=>{
-        this.lectura = data.result;
+        this.lecturas = data.result;
     };
     let storeClientes = this.abrirStore('tblclientes', 'readonly'),
         dataClientes = storeClientes.getAll();
@@ -95,22 +98,27 @@ methods:{
     };    
 },
     
-    mostrarCliente(pro){
-        this.lectura = pro;
+    mostrarLectura(lect){
+        this.lecturas = lect;
         this.accion='modificar';
     },
     limpiar(){
         this.accion='nuevo';
-        this.lectura.idLectura='';
-        this.lectura.codigo='';
-        this.lectura.nombre='';
+        this.lecturas.cliente.id=0;
+        this.lecturas.cliente.label="";
+        this.lecturas.idLectura='';
+        this.lecturas.codigo='';
+        this.lecturas.fecha='';
+        this.lecturas.lanterior='';
+        this.lecturas.lactual='';
+        this.lecturas.pago='';
        
         this.obtenerDatos();
     },
-    eliminarCliente(pro){
-        if( confirm(`Esta seguro que desea eliminar este cliente:  ${pro.nombre}`) ){
+    eliminarLectura(lect){
+        if( confirm(`Esta seguro que desea eliminar esta lectura:  ${lect.codigo}`) ){
             let store = this.abrirStore("tblLecturas",'readwrite'),
-                req = store.delete(pro.idLectura);
+                req = store.delete(lect.idLectura);
             req.onsuccess=resp=>{
                 this.mostrarMsg('Registro eliminado con exito',true);
                 this.obtenerDatos();
@@ -130,42 +138,69 @@ created(){
     //this.obtenerDatos();
 },
 template:`
-<form v-on:submit.prevent="guardarCliente" v-on:reset="limpiar">
+<form v-on:submit.prevent="guardarLectura" v-on:reset="limpiar">
 <div class="row">
     <div class="col-sm-5">
         <div class="row p-2">
-            <div class="col-sm text-center text-white btn-success">
-                <h5>REGISTRO DE LECTURAS</h5>
-            </div>
-            <div class="col-1 align-middle" >
-                                <button type="button" onclick="appVue.forms['clientes'].mostrar=false" class="btn-close" aria-label="Close"></button>
-                            </div>
-                        </div>
+            <div class="col-sm text-center text-white bg-primary">
+                <div class="row">
+                    <div class="col-11">
+                        <h5>REGISTRO DE LECTURAS</h5>
+                    </div>
+                    <div class="col-1 align-middle" >
+                        <button type="button" onclick="appVue.forms['lecturas'].mostrar=false" class="btn-close" aria-label="Close"></button>
                     </div>
                 </div>
-                <div class="row p-2">
-                        <div class="col-sm">NOMBRE:</div>
-                        <div class="col-sm">
-                            <v-select-clientes v-model="lecturas.clientes" :options="clientes" placeholder="Por favor seleccione el nombre del registro"/>
-                        </div>
-                        </div>
-                        <div class="row p-2"><!-- FUNCIONAL -->
-                            <div class="col-sm">IDCODIGO: </div>
-                            <div class="col-sm">
-                                <input v-model="lectura.idcodigo" required pattern="[A-ZÑña-z0-9 ]{5,65}" type="text" class="form-control form-control-sm" placeholder="Escriba su nombre">
-                            </div>
-        </div>
-        <div class="row p-2"><!-- FUNCIONAL -->
-            <div class="col-sm">CODIGO:</div>
-            <div class="col-sm">
-                <input v-model="lectura.codigo" required pattern="^[0-9]{4}$" type="text" class="form-control form-control-sm" placeholder="000X"" >
+            </div>
+            
+             <div class="row p-2">
+             <div class="col-sm">CLIENTE:</div>
+             <div class="col-sm">
+             <v-select-clientes v-model="lecturas.cliente" :options="clientes" placeholder="Por favor seleccione el nombre del registro"/>
+              </div>
             </div>
 
+        <div class="row p-2">
+            <div class="col-sm">FECHA: </div>
+            <div class="col-sm">
+                <input v-model="lecturas.fecha" type="date" class="form-control form-control-sm">
+        </div>
 
-        
+        </div>
+             <div class="row p-2">
+            <div class="col-sm">CODIGO:</div>
+            <div class="col-sm">
+                <input v-model="lecturas.codigo" required pattern="^[0-9]{4}$" type="text" class="form-control form-control-sm" >
+            </div>
+        </div>
+
+  
+
+        <div class="row p-2">
+            <div class="col-sm">LECTURA ANTERIOR: </div>
+            <div class="col-sm">
+                <input v-model="lecturas.lanterior" type="text" class="form-control form-control-sm">
+            </div>
+        </div>
+
+        <div class="row p-2">
+            <div class="col-sm">LECTURA ACTUAL: </div>
+            <div class="col-sm">
+                <input v-model="lecturas.lactual" type="text" class="form-control form-control-sm">
+            </div>
+        </div>
+     
+        <div class="row p-2">
+            <div class="col-sm">PAGO: </div>
+            <div class="col-sm">
+                <input v-model="lecturas.pago" type="text" class="form-control form-control-sm">
+            </div>
+        </div>
+
+        </div>
         <div class="row p-2">
             <div class="col-sm text-center">
-                <input type="submit" value="Guardar" class="btn btn-dark">
+                <input type="submit" value="Guardar" class="btn btn-success">
                 <input type="reset" value="Limpiar" class="btn btn-warning">
             </div>
         </div>
@@ -177,37 +212,42 @@ template:`
             </div>
         </div>
     </div>
-    
-    
     <div class="col-sm"></div>
     <div class="col-sm-6 p-2">
-        <div class="row text-center text-white bg-primary">
-            <div class="col"><h5> LECTURAS </h5></div>
+        <div class="row text-center text-white bg-danger">
+            <div class="col"><h5>LECTURAS REGISTRADAS</h5></div>
         </div>
-        
         <div class="row">
             <div class="col">
                 <table class="table table-sm table-hover">
                     <thead>
                         <tr>
                             <td colspan="5">
-                                <input v-model="buscar" v-on:keyup="buscandoCliente" type="text" class="form-control form-contro-sm" placeholder="Buscar Cliente">
+                                <input v-model="buscar" v-on:keyup="buscandoLectura" type="text" class="form-control form-contro-sm" placeholder="Buscar Lectura">
                             </td>
                         </tr>
                         <tr>
-                            <th>NOMBRE</th>
-                            <th>IDCODIGO</th>
+                            <th>CLIENTE</th>
+                            <th>FECHA</th>
                             <th>CODIGO</th>
+                            <th>LECTURA ANTERIOR</th>
+                            <th>LECTURA ACTUAL</th>
+                            <th>PAGO</th>
+
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="pro in lecturas" v-on:click="mostrarCliente(pro)">
-                            <td>{{ pro.nombre }}</td>
-                            <td>{{ pro.idcodigo }}</td>
-                            <td>{{ pro.codigo }}</td>
+                        <tr v-for="lect in lecturas" v-on:click="mostrarLectura(lect)">
+                            <td>{{ lect.cliente.label }}</td>
+                            <td>{{ lect.fecha }}</td>
+                            <td>{{ lect.codigo }}</td>
+                            <td>{{ lect.lanterior }}</td>
+                            <td>{{ lect.lactual }}</td>
+                            <td>{{ lect.pago }}</td>
                            
                             <td>
-                                <a @click.stop="eliminarCliente(pro)" class="btn btn-danger">DEL</a>
+                                <a @click.stop="eliminarLectura(lect)" class="btn btn-danger">DEL</a>
                             </td>
                         </tr>
                     </tbody>
